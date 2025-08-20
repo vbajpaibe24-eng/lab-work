7ed5ce78c3fac3bc3e4e3864dd8029fc6a97d8d2
Bluesyspyder <vbajpai_be24@thapar.edu>
2025-08-20 16:03:52 +0530

    Update Q2.cpp

--- DSA lab work - 3/Q2.cpp
+++ DSA lab work - 3/Q2.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
@@ -15,9 +16,14 @@ void bubbleSort(int arr[], int n) {
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
+
     bubbleSort(arr, n);
-    printf("Sorted array: ");
+
+    cout << "Sorted array: ";
     for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        cout << arr[i] << " ";
+
+    cout << endl;
     return 0;
 }
+
